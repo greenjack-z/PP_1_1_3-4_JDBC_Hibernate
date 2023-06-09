c202a0e545fedb44b659bc71bb0a631dbb75cbb5
Evgeniy Zelyanin <e.zelyanin@gmail.com>
2023-06-09 22:53:58 +0300

    add hibernate DAO implementation

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,96 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private final Logger logger = Logger.getLogger(UserDaoHibernateImpl.class.getName());
+    public UserDaoHibernateImpl() {
+        //Empty constructor;
     }
 
 
     @Override
     public void createUsersTable() {
-
+        String query = """
+                    CREATE TABLE IF NOT EXISTS users (
+                    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                    name VARCHAR(45) NOT NULL,
+                    lastName VARCHAR(45) NOT NULL,
+                    age TINYINT UNSIGNED NOT NULL
+                );
+                """;
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createNativeQuery(query).executeUpdate();
+            session.getTransaction().commit();
+            logger.info("Table created.");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String query = "DROP TABLE IF EXISTS users";
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createNativeQuery(query).executeUpdate();
+            session.getTransaction().commit();
+            logger.info("Table deleted.");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        User user = new User(name, lastName, age);
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            logger.info("User added.");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            session.getTransaction().commit();
+            logger.info("User deleted.");
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+            CriteriaQuery<User> all = criteriaQuery.select(root);
+            TypedQuery<User> typedQuery = session.createQuery(all);
+            return typedQuery.getResultList();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSession()) {
+            String hql = "delete from User";
+            Query<User> query = session.createQuery(hql);
+            session.beginTransaction();
+            query.executeUpdate();
+            session.getTransaction().commit();
+            logger.info("Table cleared.");
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class Util {
@@ -24,5 +30,20 @@ public class Util {
         } catch (SQLException e) {
             throw new IllegalStateException("Cannot connect the database!", e);
         }
+    }
+
+    public static Session getSession() {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        properties.put(AvailableSettings.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(AvailableSettings.URL, URL);
+        properties.put(AvailableSettings.USER, USERNAME);
+        properties.put(AvailableSettings.PASS, PASSWORD);
+
+        configuration.setProperties(properties);
+        configuration.addAnnotatedClass(User.class);
+        return configuration.buildSessionFactory().openSession();
     }
 }
